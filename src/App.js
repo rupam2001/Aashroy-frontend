afd777f4fd33ed3040cb9269ca96cd140f72cb3b
Devzard <debashishgogoi999@gmail.com>
2021-09-16 19:19:03 +0530

    report homeless form in progress

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthContext } from "./contexts/auth.context";
-import GeneralUserLogin from "./routes/generalUserLogin";
+import { getAccessToken, getRefreshToken } from "./utils/storage";
+
+import GeneralUser from "./routes/GeneralUser";
+import GeneralUserLogin from "./routes/GeneralUser/GeneralUserLogin";
+import ReportHomeless from "./routes/GeneralUser/ReportHomeless";
+
 import NGORegistration from "./routes/ngo/registration";
 
 import Landing from "./routes/landing";
 // global arrayContainer:true, SliderInstance:true, DomObjects:true, document, Slider
 
 function App() {
+  const authcontext = useContext(AuthContext);
+  useEffect(() => {
+    if (getAccessToken() != null && getRefreshToken() != null)
+      authcontext.setIsLoggedIn(true);
+  }, []);
+
   return (
     <div>
       <GlobalRoutes />
@@ -24,7 +35,7 @@ function GlobalRoutes() {
     <Router>
       <Switch>
         <Route path="/" exact component={Landing} />
-        <Route path="/general/login" component={GeneralUserLogin} />
+        <Route path="/general/login" exact component={GeneralUserLogin} />
         <Route path="/ngo/registration" component={NGORegistration} />
       </Switch>
     </Router>
@@ -36,10 +47,17 @@ function AuthSecureRoutes() {
    * Place all the secure routes here
    */
   const authcontext = useContext(AuthContext);
-  if (!authcontext.isLoggedIn) return <></>;
+  // if (!authcontext.isLoggedIn) return <></>;
   return (
     <Router>
-      <Switch></Switch>
+      <Switch>
+        <Route path="/general" exact component={GeneralUser} />
+        <Route
+          path="/general/report-homeless"
+          exact
+          component={ReportHomeless}
+        />
+      </Switch>
     </Router>
   );
 }
--- src/layouts/user.layout.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function UserLayout({ children }) {
-  return <div>{children}</div>;
-}
