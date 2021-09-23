6a7a978dbeda6fe644b4b009b868aef465f23bac
RG-404 <55778353+RG-404@users.noreply.github.com>
2021-09-23 17:20:06 +0530

    add marker prop support

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,20 +1,21 @@
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-import geoJson from "./chicago-parks.json"
 import "./style.css";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoicmctNDA0IiwiYSI6ImNrczk5eG8yZzF1dmgydnBoZWgwNjZzZ2QifQ.hrxTaZwJoKGMxstCagV5zw";
 
-export default function App() {
+const Map = ({ region, markers }) => {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+  const [lng, setLng] = useState(94.2122044);
+  const [lat, setLat] = useState(26.743573);
+  const [zoom, setZoom] = useState(13);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
+    setLng(region[0]);
+    setLat(region[1]);
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v11",
@@ -22,13 +23,25 @@ export default function App() {
       zoom: zoom,
     });
     map.current.addControl(new mapboxgl.NavigationControl(), "top-right");
+    console.log(markers);
+
+    markers.map((marker) => {
+      const el = document.createElement("div");
+      el.setAttribute("title", "Reported Area");
+      el.className = "marker-custom";
+      new mapboxgl.Marker(el)
+        .setLngLat([marker.longitude, marker.latitude])
+        .setPopup(
+          new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML(`<h3>Hello</h3><p>${marker.address}</p>`)
+        )
+        .addTo(map.current);
+    });
   });
 
   useEffect(() => {
     if (!map.current) return; // wait for map to initialize
-    geoJson.features.map((feature) =>
-      new mapboxgl.Marker().setLngLat(feature.geometry.coordinates).addTo(map.current)
-    );
+
     map.current.on("move", () => {
       setLng(map.current.getCenter().lng.toFixed(4));
       setLat(map.current.getCenter().lat.toFixed(4));
@@ -37,4 +50,6 @@ export default function App() {
   });
 
   return <div ref={mapContainer} className="map-container" />;
-}
+};
+
+export default Map;
--- src/routes/test/index.js
+++ src/routes/test/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import Map from "../../components/Map";
 
+const markers = [
+  {
+    address: "123",
+    latitude: 26.743573,
+    longitude: 94.2122044,
+  },
+  {
+    address: "cps e",
+    latitude: 26.733573,
+    longitude: 94.2322044,
+  },
+  {
+    address: "asdf a",
+    latitude: 26.732573,
+    longitude: 94.2022044,
+  },
+];
+
 const TesingComponent = () => {
   return (
     <div className="h-screen w-screen bg-purple-600">
-      <Map />
+      <Map region={[94.2142044, 26.753573]} markers={markers} />
     </div>
   );
 };
