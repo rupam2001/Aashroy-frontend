3cad310d70e3c8a24734aa106a95cb80567b3241
Devzard <debashishgogoi999@gmail.com>
2021-09-26 15:23:59 +0530

    added more donation option

--- src/constants/donation.constants.js
+++ src/constants/donation.constants.js
@@ -1,4 +1,4 @@
-const DONATION_TYPES = ["clothes", "food", "medicine"];
+const DONATION_TYPES = ["clothes", "food", "medicine", "money", "others"];
 const DONATION_PHOTO_LIMIT = 3;
 
 export { DONATION_TYPES, DONATION_PHOTO_LIMIT };
